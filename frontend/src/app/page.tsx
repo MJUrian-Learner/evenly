50910c1483ae4f3d3eb6cdbc83f8b4766b0ccacf
Michael James Urian <businessmjurian@gmail.com>
2025-01-19 18:23:20 +0800

    feat: enhance Home component with buttons for protected and unprotected calls, and sign out functionality

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Evenly</div>;
+  return (
+    <div className="space-x-2">
+      <Button>Call Protected</Button>
+      <Button>Call Unprotected</Button>
+      <SignOutButton>
+        <Button>Sign Out</Button>
+      </SignOutButton>
+    </div>
+  );
 }
